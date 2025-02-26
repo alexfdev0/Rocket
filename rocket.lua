fb96dfcfc8bfbf6c2fffa5745e76e4501c0ee243
Alex Flax <alexflax772@gmail.com>
2025-02-26 22:42:11 +0000

    Version 2 + Rename to rocket since "ascript" is taken

--- rocket.lua
+++ rocket.lua
@@ -1,10 +1,6 @@
-local variables = {
+local variables = {}
 
-}
-
-local functions = {
-
-}
+local functions = {}
 
 local validsecondclass = {
     "=",
@@ -32,7 +28,11 @@ local errors = {
     [11] = "File could not be read",
     [12] = "Not a valid number", 
     [13] = "Not a valid arithmetic operation",
-    [14] = "Division by zero is not allowed"
+    [14] = "Division by zero is not allowed",
+    [15] = "String is either not opened or closed by quotes",
+    [16] = "Variable does not exist",
+    [17] = "Value not supplied",
+    [18] = "Illegal comparison"
 }
 
 function throwNew(typeo, error, args)
@@ -59,9 +59,209 @@ function checkArgs(expct, tokens)
     end
 end
 
-local readText
+function getValueFromVariable(variable)
+    if variables[variable] then
+        return variables[variable]
+    else
+        return nil
+    end
+end
 
-readText = function(text, args)
+function parseInput(valstart, tokens, checkfor, tokenassoc)
+    --[[
+    example: {"y", "=", "1", "+", "1"} (y = 1 in this istance)
+    We need to parse it so it is {"y", "=", "2"}
+    ]]
+    local function solve(fnum, op, lnum)
+        if op == "+" then
+            return fnum + lnum
+        elseif op == "-" then
+            return fnum - lnum
+        elseif op == "*" then
+            return fnum * lnum
+        elseif op == "/" then
+            return fnum / lnum
+        elseif op == "^" then
+            return fnum ^ lnum
+        elseif op == "=" then
+            if fnum == lnum then
+                return true
+            else
+                return false
+            end
+        end
+    end
+    -- Check for lone variable
+    if valstart == #tokens and checkfor == true then
+        if getValueFromVariable(tokens[valstart]) then
+            return getValueFromVariable(tokens[valstart])
+            -- If not then assume it is a string being passed and move it along.
+        end
+    end
+    -- Check for arithmetic operation
+    local opsect = tokens[valstart + 1]
+    if opsect == "+" or opsect == "-" or opsect == "*" or opsect == "/" or opsect == "^" then
+        local fnum = tokens[valstart]
+        local lnum = tokens[valstart + 2]
+        -- Check for if they are numbers or variables
+        if tonumber(fnum) then
+            fnum = tonumber(fnum)
+        else
+            if tonumber(getValueFromVariable(fnum)) then
+                fnum = tonumber(getValueFromVariable(fnum))
+            else
+                if tonumber(getValueFromVariable(fnum)) ~= nil then
+                    throwNew("error", 12, "")
+                else
+                    throwNew("error", 16, "")
+                end
+            end
+        end
+        if tonumber(lnum) then
+            lnum = tonumber(lnum)
+        else
+            if tonumber(getValueFromVariable(lnum)) then
+                lnum = tonumber(getValueFromVariable(lnum))
+            else
+                if getValueFromVariable(lnum) ~= nil then
+                    throwNew("error", 12, "")
+                else
+                    throwNew("error", 16, "")
+                end
+            end
+        end
+        return solve(fnum, opsect, lnum)
+    end
+    -- Check for lone numbers
+    if tonumber(tokens[valstart]) then
+        checkArgs(valstart, tokens)
+        return tonumber(tokens[valstart])
+    end
+    -- Check for strings
+    if valstart == #tokens then
+        if string.find(tokens[valstart], '"') then
+            local count = 0
+
+            for _ in string.gmatch(tokens[valstart], '"') do
+                count = count + 1
+            end
+
+            if count < 2 then
+                throwNew("error", 15, "")
+            end
+
+            tokens[valstart] = tokens[valstart]:gsub('"', "")
+        else
+            if getValueFromVariable(tokens[valstart]) then
+                tokens[valstart] = getValueFromVariable(tokens[valstart])
+            else
+                throwNew("error", 16, "")
+            end
+        end
+        return tokens[valstart]
+    else
+        local valend = 0
+        for i = valstart + 1, #tokens do
+            if string.find(tokens[i], '"') then
+                valend = i
+                break
+            end
+        end
+        if valend == 0 then
+            throwNew("error", 15, "")
+        end
+        tokens[valstart] = tokens[valstart]:gsub('"', "")
+        tokens[valend] = tokens[valend]:gsub('"', "")
+        local str = ""
+        for i = valstart, valend do
+            if i == valstart then
+                str = str .. tokens[i]
+            else
+                str = str .. " " .. tokens[i]
+            end
+        end
+        return str
+    end
+end
+
+local function compare(tokens, condstart, condend)
+    local format = 0
+    if tokens[condstart + 1] == "=" then
+        format = 1
+    else
+        format = 2
+    end
+    if format == 1 then
+        local firsttokens = {}
+        local secondtokens = {}
+        local equalloc = 0
+
+        for i = condstart, #tokens do
+            if tokens[i] ~= "=" then
+                table.insert(firsttokens, tokens[i])
+            else
+                equalloc = i
+                break
+            end
+        end
+
+        local firstres = parseInput(1, firsttokens)
+
+        if equalloc == 0 then
+            throwNew("error", 18, "")
+        end
+
+        for i = equalloc + 1, condend do
+            table.insert(secondtokens, tokens[i])
+        end
+
+        local secondres = parseInput(1, secondtokens)
+
+        if firstres == secondres then
+            return true
+        else
+            return false
+        end
+    else
+        local f = tokens[condstart]
+
+        local restokens = {}
+        local exptokens = {}
+        local equalloc = 0
+        local thenloc = condend + 1
+        
+        for i = condstart, #tokens do
+            if tokens[i] ~= "=" then
+                table.insert(restokens, tokens[i])
+            else
+                equalloc = i
+                break
+            end
+        end
+
+        local first = parseInput(1, restokens)
+
+        if equalloc == 0 then
+            throwNew("error", 18, "")
+        end
+
+        for i = equalloc + 1, condend do
+            table.insert(exptokens, tokens[i])
+        end
+
+        local second = parseInput(1, exptokens)
+
+        if first == second then
+            return true
+        else
+            return false
+        end
+    end
+end
+
+local interpret
+
+interpret = function(text, args)
     if args == nil then
         args = {}
     end
@@ -86,10 +286,19 @@ readText = function(text, args)
     end
 
     if tokens[1] == "if" or tokens[1] == "function" then
-        for j = 1, #tokens do
-            if tokens[j] == "end;" then
-                tokens[j] = tokens[j]:gsub(";", "")
-                splitloc = j
+        local depth = 0
+        
+        for i = 1, #tokens do
+            if tokens[i] == "function" or tokens[i] == "if" then
+                depth = depth + 1
+            elseif tokens[i] == "end;" then
+                depth = depth - 1
+
+                if depth == 0 then
+                    tokens[i] = tokens[i]:gsub(";", "")
+                    splitloc = i
+                    break
+                end
             end
         end
     else
@@ -136,18 +345,9 @@ readText = function(text, args)
     if func == true then
         local name = tokens[1]
         if name == "print" then
-            if variables[tokens[2]] then
-                print(variables[tokens[2]])
-            else
-                local str = ""
-                for i = 2, #tokens do
-                    if i == 2 then
-                        str = str .. tokens[i]
-                    else
-                        str = str .. " " .. tokens[i]
-                    end
-                end
-                print(str)
+            local res = parseInput(2, tokens, true)
+            if res then
+                print(res)
             end
         elseif name == "if" then
             -- if statement handling
@@ -181,42 +381,24 @@ readText = function(text, args)
             local funcstart = tokens[thenloc + 1]
             local funcend = tokens[endloc - 1]
 
-            -- Address = statements
             if condstart == "=" then
                 throwNew("error", 5, "")
             end
 
-            if tokens[2 + 1] == "=" then
-                local thing1 = nil
-                local thing2 = nil
-                if variables[condstart] then
-                    thing1 = variables[condstart]
-                else
-                    thing1 = condstart
-                end
-
-                if tokens[2 + 2] == "then" then
-                    throwNew("error", 6, "")
-                end
-
-                if variables[tokens[2 + 2]] then
-                    thing2 = variables[tokens[2 + 2]]
-                else
-                    thing2 = tokens[2 + 2]
-                end
-
-
-                if thing1 == thing2 then
-                    local portion = ""
-                    for i = thenloc + 1, endloc - 1 do
-                        if i == thenloc + 1 then
-                            portion = portion .. tokens[i]
-                        else
-                            portion = portion .. " " .. tokens[i]
-                        end
+            local execFunc = function()
+                local portion = ""
+                for i = thenloc + 1, endloc - 1 do
+                    if i == thenloc + 1 then
+                        portion = portion .. tokens[i]
+                    else
+                        portion = portion .. " " .. tokens[i]
                     end
-                    readText(portion)
                 end
+                interpret(portion)
+            end
+
+            if compare(tokens, 2, thenloc - 1) then
+                execFunc()
             end
         elseif name == "function" then
             local fname = tokens[2]
@@ -280,7 +462,7 @@ readText = function(text, args)
                     end
                     variables[argname] = relevantval
                 end
-                readText(functions[fname][1], functions[fname][2])
+                interpret(functions[fname][1], functions[fname][2])
                 for i, arg in pairs(args) do
                     variables[arg] = nil
                 end
@@ -326,8 +508,18 @@ readText = function(text, args)
     -- Decleration handler
     if vdec == true then
         if tokens[2] == "=" then
-            checkArgs(3, tokens)
-            variables[tokens[1]] = tokens[3]
+            local vname = tokens[1]
+            local valstart = 3
+
+            if not tokens[3] then
+                throwNew("error", 17, "")
+            end
+            
+            local res = parseInput(valstart, tokens)
+
+            if res then
+                variables[vname] = res
+            end
         end
     end
 
@@ -343,16 +535,16 @@ readText = function(text, args)
             end
         end
 
-        readText(str, {})
+        interpret(str, {})
     end
 end
 
 local exec
 
 exec = function()
-    io.write("ascript > ")
+    io.write("rocket > ")
     local string = io.read("*l")
-    readText(string)
+    interpret(string)
     exec()
 end
 
@@ -364,9 +556,9 @@ if filename ~= nil then
     end
     local content = file:read("*a")
     file:close()
-    readText(content)
+    interpret(content)
 else
-    print("AScript version 1.01")
+    print("Rocket version 2")
     local os = os.getenv("OS")
     if os then
         print("OS: " .. os)
